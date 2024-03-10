96e4fe1665550e9be3a5fafddb581035bddce98f
IlyaG96 <ilyag_96@icloud.com>
2024-03-10 21:31:13 +0300

    + del: deleted tg handler from app routs

--- src/rest_service/routes/routes.py
+++ src/rest_service/routes/routes.py
@@ -1,11 +1,9 @@
 from aiohttp import web
 from rest_service.handlers.model_service_handler import service_model_incoming_handler
 from rest_service.handlers.log_handler import debug_log_handler
-from rest_service.handlers.telegram_handler import telegram_incoming_handler
 
 
 def setup_routes(app: web.Application):
-    app.router.add_post('/tm', telegram_incoming_handler)
     app.router.add_post('/post', service_model_incoming_handler)
     app.router.add_get('/cm', service_model_incoming_handler)
     app.router.add_post('/log', debug_log_handler)
