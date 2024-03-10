571580ddac6e801a6ef9eaf26d717caa1e649c12
IlyaG96 <ilyag_96@icloud.com>
2024-03-10 21:30:55 +0300

    + del: deleted unused files

--- src/telegram_bot/handlers/__init__.py
+++ src/telegram_bot/handlers/__init__.py
@@ -2,7 +2,9 @@ from telethon import TelegramClient
 from telethon import events
 
 from .start_handler import start_handler
+from .second_message_handler import second_message_handler
 
 
 def setup_handlers(bot: TelegramClient):
-    bot.add_event_handler(start_handler, events.NewMessage())
+    bot.add_event_handler(start_handler, events.NewMessage(pattern='/start'))
+    bot.add_event_handler(second_message_handler, events.NewMessage(pattern="далее"))
